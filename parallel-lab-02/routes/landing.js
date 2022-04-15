f4ce918191bd52ec18f6e78e70a260653986059c
neomq <neo.minqi@trentglobal.edu.sg>
2022-04-15 10:15:51 +0000

    updated parallel lab 2

--- parallel-lab-02/routes/landing.js
+++ parallel-lab-02/routes/landing.js
@@ -3,6 +3,7 @@ const router = express.Router(); // #1 - Create a new express Router
 
 
 //  #2 Add a new route to the Express router
+
 router.get('/', (req,res)=>{
     res.render('landing/index')
 })
